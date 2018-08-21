570b34032383e38c13d682fe0891f6200c4a4f39
bsjodin <42284156+bsjodin@users.noreply.github.com>
2018-08-21 15:34:49 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+
+chmod 755 madmixture
+
+if [[ -d /usr/local/bin ]];then
+	cp -i madmixture /usr/local/bin
+	echo "madmixture successfully installed."
+else
+	echo "/usr/local/bin doesn't not exist! Editing PATH instead."
+	if [[ -f ~/.bashrc ]];then
+		echo "export PATH=$PWD:\$PATH" >> ~/.bashrc
+		source ~/.bashrc
+		echo ".bashrc appended."
+	fi
+	if [[ -f ~/.bash_profile ]];then
+		echo "export PATH=$PWD:\$PATH" >>  ~/.bash_profile
+		eval source ~/.bash_profile
+		echo ".bash_profile appended."
+	fi
+	if [[ -f ~/.profile ]];then
+		echo "export PATH=$PWD:\$PATH" >> ~/.profile
+		eval source ~/.profile
+		echo ".profile appended."
+	fi
+	if [[ -f ~/.bash_profile || -f ~/.bashrc || -f ~/.profile ]];then
+		echo;echo "madmixture successfully installed."
+	else
+		echo "ERROR: Could not install madmixture! Please use 'bash madmixture' to run."
+	fi
+fi
